387441023df8b088811535b99d0767e4e789971d
ppogreba <153020124+ppogreba@users.noreply.github.com>
2025-06-08 22:27:35 -0700

    Update fan_control.sh

--- fan_control.sh
+++ fan_control.sh
@@ -12,64 +12,55 @@ HOST_IP="192.168.1.120"
 fan_speed=1
 while true
 do
+    MAX=false
+    HIGH=false
+    MID=false
+    low=False
+    NORM=false
     temps=()
     for ((i=0; i<$NUMBER_GPUS; i++)); do
         temps+=($(nvidia-smi --id=$i -q --display=TEMPERATURE | grep GPU.Current.Temp | awk '{print $5}'))
     done  
-    echo "${temps[@]}"
+    echo -e "\n${temps[0]}   ${temps[1]}"
     all_less_than_50=true
     for temp in ${temps[@]}; do
         if [ $temp -ge 85 ]; then
-            update_speed=100
-            if [ $update_speed -ne $fan_speed ]; then
-                echo "fan speed MAX"
-                fan_speed=$update_speed
-                ssh -i /etc/ssh/id_rsa.pem automation@$HOST_IP << EOF
-                    racadm set System.ThermalSettings.MinimumFanSpeed $fan_speed
-                    exit
-EOF
-            fi
+            MAX=true
+            echo -n "MAX! "
         elif [ $temp -ge 75 ] && [ $temp -lt 85 ]; then
-            update_speed=70           
-            if [ $update_speed -ne $fan_speed ]; then
-                echo "fan speed HIGH"
-                fan_speed=$update_speed
-                ssh -i /etc/ssh/id_rsa.pem automation@$HOST_IP << EOF
-                    racadm set System.ThermalSettings.MinimumFanSpeed $fan_speed
-                    exit
-EOF
-            fi
+            HIGH=true
+            echo -n "HIGH "         
         elif [ $temp -ge 60 ] && [ $temp -lt 75 ]; then
-            update_speed=50           
-            if [ $update_speed -ne $fan_speed ]; then
-                echo "fan speed Medium"
-                fan_speed=$update_speed
-                ssh -i /etc/ssh/id_rsa.pem automation@$HOST_IP << EOF
-                    racadm set System.ThermalSettings.MinimumFanSpeed $fan_speed
-                    exit
-EOF
-            fi
+            MID=true
+            echo -n "Mid  "         
         elif [ $temp -ge 50 ] && [ $temp -lt 60 ]; then
-            update_speed=30            
-            if [ $update_speed -ne $fan_speed ]; then
-                echo "fan speed low"
-                fan_speed=$update_speed
-                ssh -i /etc/ssh/id_rsa.pem automation@$HOST_IP << EOF
-                    racadm set System.ThermalSettings.MinimumFanSpeed $fan_speed
-                    exit
-EOF
-            fi
-        else
-            update_speed=0
-            if [ $update_speed -ne $fan_speed ]; then
-                echo "fan speed normal"
-                fan_speed=$update_speed
-                ssh -i /etc/ssh/id_rsa.pem automation@$HOST_IP << EOF 
-                    racadm set System.ThermalSettings.MinimumFanSpeed $fan_speed
-                    exit
-EOF
-            fi
+            LOW=true
+            echo -n "Low  "
+        elif [ $temp -ge 0 ] && [ $temp -lt 50 ]; then
+            NORM=true
+            echo -n "Norm "      
         fi
     done
+    if $MAX; then
+        update_speed=100
+    elif $HIGH; then
+        update_speed=70
+    elif $MID; then
+        update_speed=50
+    elif $LOW; then
+        update_speed=30
+    elif $NORM; then
+        update_speed=0
+    fi
+    if [ $update_speed -ne $fan_speed ]; then
+        echo -e "\nUpdate Fan Speed to $update_speed"
+        fan_speed=$update_speed
+        ssh -i /etc/ssh/id_rsa.pem automation@$HOST_IP << EOF 
+        racadm set System.ThermalSettings.MinimumFanSpeed $fan_speed
+        exit
+EOF
+    else
+        sleep 2
+    fi
 done
 exit 0
